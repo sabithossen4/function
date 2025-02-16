08fa392a82ca42f8fb58d7a0c544e4f754c74f8d
sabithossen4 <sabithossenkhoka420@gmail.com>
2025-02-16 11:25:25 +0600

    added a unique file.

--- no-duplicate.js
+++ no-duplicate.js
@@ -1,14 +1,16 @@
 const biraniKhor=['abul','babul','kabul','abul','babul','bulbul','kobul','kobul'];
 const numbers=[45,98,21,68,45,5,7,5,8];
-const unique=[];
+
 function dublicateArry(arry){
+  const unique=[];
   for(const item of arry){
-     if(unique.includes(item)===false){
+     if(!unique.includes(item)){
          unique.push(item);
      }
   }
   return unique;
 }
 const unickArry=dublicateArry(biraniKhor);
-// const unickArry=dublicateArry(numbers);
+const unickArry2=dublicateArry(numbers);
 console.log(unickArry);
+// console.log(unickArry2);
